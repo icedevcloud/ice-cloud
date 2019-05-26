54e5fda99672db844a348dd0de3a8e341f1e621d
XiaoBingBy <XiaoBingBy@Gmail.com>
2019-05-26 23:43:32 +0800

    [新增] bing-test 模块

---
+++ bing-test/src/main/java/com/xiaobingby/test/BingTestApplication.java
@@ -0,0 +1,13 @@
+package com.xiaobingby.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BingTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BingTestApplication.class, args);
+    }
+
+}
