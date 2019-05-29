a305449f42476df949aa7062b913245ddbcba784
XiaoBingBy <XiaoBingBy@Gmail.com>
2019-05-29 21:32:58 +0800

    [修改] 放行url

--- bing-resources/src/main/java/com/xiaobingby/resources/config/ResourceServerConfigurer.java
+++ bing-resources/src/main/java/com/xiaobingby/resources/config/ResourceServerConfigurer.java
@@ -24,6 +24,7 @@ public class ResourceServerConfigurer extends ResourceServerConfigurerAdapter {
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
+                .antMatchers("/test/**").permitAll()
                 .anyRequest().authenticated()
                 .and().csrf().disable();
     }
