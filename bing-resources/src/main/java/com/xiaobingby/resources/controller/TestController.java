a44db959913021981843f2403cfd9ce616faf29b
XiaoBingBy <XiaoBingBy@Gmail.com>
2019-05-29 22:04:15 +0800

    [增加] 测试Controller

---
+++ bing-resources/src/main/java/com/xiaobingby/resources/controller/TestController.java
@@ -0,0 +1,29 @@
+package com.xiaobingby.resources.controller;
+
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/test")
+@RestController
+@RefreshScope
+public class TestController {
+
+    @Value("${value}")
+    private String value;
+
+    @GetMapping("/value")
+    public String value() {
+        return value;
+    }
+
+    @GetMapping(value = "/hello")
+    @SentinelResource("hello")
+    public String hello() {
+        return "Hello Sentinel";
+    }
+
+}
