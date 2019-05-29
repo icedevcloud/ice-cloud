57968c1fc87f9628c1e7729c63f68862157a624e
XiaoBingBy <XiaoBingBy@Gmail.com>
2019-05-29 21:32:42 +0800

    [修改] 增加阿里巴巴nacos为配置中心注册中心

--- bing-resources/src/main/java/com/xiaobingby/resources/controller/IndexController.java
+++ bing-resources/src/main/java/com/xiaobingby/resources/controller/IndexController.java
@@ -1,5 +1,6 @@
 package com.xiaobingby.resources.controller;
 
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +10,16 @@ public class IndexController {
     @GetMapping("/")
     public String index() {
         return "index";
+    }
+
+    @GetMapping("/cache")
+    public String cache() {
+        return cacheData();
+    }
+
+    @Cacheable("cache")
+    public String cacheData() {
+        return "hello cache";
     }
 
 }
