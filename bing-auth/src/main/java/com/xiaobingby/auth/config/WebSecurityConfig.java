6df82766d56ace61940e03ca701de49da30d034d
XiaoBingBy <XiaoBingBy@Gmail.com>
2019-06-01 14:13:10 +0800

    [修改] bing-auth

--- bing-auth/src/main/java/com/xiaobingby/auth/config/WebSecurityConfig.java
+++ bing-auth/src/main/java/com/xiaobingby/auth/config/WebSecurityConfig.java
@@ -19,7 +19,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers(
-                        "/xxx/**").permitAll()
+                        "/actuator/**").permitAll()
                 .anyRequest().authenticated()
                 .and().csrf().disable();
     }
