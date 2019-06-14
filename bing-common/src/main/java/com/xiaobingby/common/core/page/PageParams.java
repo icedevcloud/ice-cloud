350ed3af3af819a2723bc1b2f390006fbbd13207
XiaoBingBy <XiaoBingBy@Gmail.com>
2019-06-14 12:05:06 +0800

    [增加] bing-common 公共类

---
+++ bing-common/src/main/java/com/xiaobingby/common/core/page/PageParams.java
@@ -0,0 +1,30 @@
+package com.xiaobingby.common.core.page;
+
+import lombok.Data;
+
+import java.util.ArrayList;
+
+/**
+ * @author XiaoBingBy
+ * @date 2018-09-05 11:10
+ * @since 1.0
+ */
+@Data
+public class PageParams {
+
+    /**
+     * 每页显示条数，默认 10
+     */
+    private long pageSize = 10;
+
+    /**
+     * 当前页
+     */
+    private long current = 1;
+
+    /**
+     * 请求条件
+     */
+    private ArrayList<Query> querys;
+
+}
---
+++ bing-common/src/main/java/com/xiaobingby/common/security/dto/UserDetailsDto.java
@@ -0,0 +1,19 @@
+package com.xiaobingby.common.security.dto;
+
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.Collection;
+
+@Data
+public class UserDetailsDto extends User {
+
+    private Long id;
+
+    public UserDetailsDto(Long id, String username, String password, boolean enabled, boolean accountNonExpired, boolean credentialsNonExpired, boolean accountNonLocked, Collection<? extends GrantedAuthority> authorities) {
+        super(username, password, enabled, accountNonExpired, credentialsNonExpired, accountNonLocked, authorities);
+        this.id = id;
+    }
+
+}
