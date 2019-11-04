962b02109c482bb4393692ac31108998fdfb0e6e
XiaoBingBy <XiaoBingBy@Gmail.com>
2019-11-04 11:09:14 +0800

    refactor: 修改全局异常处理

--- ice-upms/ice-upms-service/src/main/java/com/icedevcloud/upms/exception/GlobalExceptionHandler.java
+++ ice-upms/ice-upms-service/src/main/java/com/icedevcloud/upms/exception/GlobalExceptionHandler.java
@@ -2,17 +2,20 @@ package com.icedevcloud.upms.exception;
 
 import com.icedevcloud.common.core.api.R;
 import com.icedevcloud.common.core.exception.ApiException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ResponseBody
     @ExceptionHandler(value = ApiException.class)
-    public R apiExceptionHandler(ApiException e) {
-        return R.failed(e.getErrorCode());
+    public R apiExceptionHandler(Exception e) {
+        if (e instanceof ApiException) {
+            ApiException apiException = (ApiException) e;
+            return R.failed(apiException.getErrorCode());
+        } else {
+            return R.failed(e.getMessage());
+        }
     }
 
 }
