42094ad6e3d913a1ff46ead6fcbeadf098b61585
XiaoBingBy <XiaoBingBy@Gmail.com>
2019-11-23 13:16:52 +0800

    refactor: 所有的枚举类型字段必须要有注释，说明每个数据项的用途。

--- ice-common/src/main/java/com/icedevcloud/common/core/api/ErrorCodeEnum.java
+++ ice-common/src/main/java/com/icedevcloud/common/core/api/ErrorCodeEnum.java
@@ -2,7 +2,13 @@ package com.icedevcloud.common.core.api;
 
 public enum ErrorCodeEnum implements IErrorCode {
 
+    /**
+     * 成功
+     */
     SUCCESS(200, "执行成功"),
+    /**
+     * 失败
+     */
     FAILED(500, "操作失败");
 
     private Integer code;
