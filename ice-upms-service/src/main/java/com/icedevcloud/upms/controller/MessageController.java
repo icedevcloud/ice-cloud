cc3aed560a49f779109c946f5a45f8c11998c25c
XiaoBingBy <XiaoBingBy@Gmail.com>
2019-12-26 22:07:21 +0800

    refactor: 单体应用改造

---
+++ ice-upms-service/src/main/java/com/icedevcloud/upms/controller/MessageController.java
@@ -0,0 +1,21 @@
+package com.icedevcloud.upms.controller;
+
+import com.icedevcloud.common.core.api.R;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Api(value = "消息管理接口类", tags = "消息管理", description = "消息管理")
+@RestController
+@RequestMapping("/upms/message")
+public class MessageController {
+
+    @ApiOperation(value = "获取消息数据接口", notes = "获取消息数据接口", produces = "application/json")
+    @GetMapping("/count")
+    public R<Integer>  messageCount() {
+        return R.ok(1);
+    }
+
+}
