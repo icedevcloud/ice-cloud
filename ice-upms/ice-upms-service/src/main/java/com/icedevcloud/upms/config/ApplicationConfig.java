b487b3fe0ab7d69456c70ad0f9c6a745cf4eaa4c
XiaoBingBy <XiaoBingBy@Gmail.com>
2020-01-09 19:38:35 +0800

    feat: swagger ui 支持

---
+++ ice-upms/ice-upms-service/src/main/java/com/icedevcloud/upms/config/ApplicationConfig.java
@@ -0,0 +1,16 @@
+package com.icedevcloud.upms.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class ApplicationConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("doc.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+}
