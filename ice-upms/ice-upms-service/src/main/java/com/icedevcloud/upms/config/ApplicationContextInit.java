5ed052bc641e6e13c2e62a73c6c7940d3932fb6d
XiaoBingBy <xiaobingby@gmail.com>
2020-05-11 21:43:52 +0800

    refactor: Spring 相关辅助类

--- ice-upms/ice-upms-service/src/main/java/com/icedevcloud/upms/config/ApplicationContextInit.java
+++ ice-upms/ice-upms-service/src/main/java/com/icedevcloud/upms/config/ApplicationContextInit.java
@@ -1,6 +1,6 @@
 package com.icedevcloud.upms.config;
 
-import com.icedevcloud.common.core.api.SpringHelper;
+import com.icedevcloud.common.core.spring.SpringHelper;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
